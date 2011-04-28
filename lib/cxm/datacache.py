8cadd39b7c6a72f940023a6cf197116432a0e187
Nicolas AGIUS <nicolas.agius@lps-it.fr>
2011-04-28 14:56:30 +0200

    Added DataCache

---
+++ lib/cxm/datacache.py
@@ -0,0 +1,102 @@
+# -*- coding:Utf-8 -*-
+
+# cxm - Clustered Xen Management API and tools
+# Copyleft 2011 - Nicolas AGIUS <nagius@astek.fr>
+# $Id:$
+
+###########################################################################
+#
+# This file is part of cxm.
+#
+# cxm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###########################################################################
+
+import time
+import core
+
+class DataCache(object):
+	
+	"""This class is a cache for any type of data with a small lifetime."""
+
+	def __init__(self):
+		self._data = dict()
+
+	def add(self, key, lifetime, value): 
+		"""
+		Add a new value to the cache.
+		 - 'key' could be any hashable object, but a significant string is better,
+		 - 'lifetime' is in second,
+		 - 'value' could be an object or any type.
+		"""
+		self._data[key] = {
+				'expire': int(time.time())+lifetime,
+				'value': value
+			}
+		core.debug("[CAH]", "ADD", key, lifetime, value)
+
+	def get(self, key):
+		"""
+		Return the cached value of the specified key.
+		If the value is outdated, it will be deleted from cache and a CacheExpiredException 
+		will be raised.
+		If the key is unknown, a CacheMissingException will be raised.
+		"""
+		try:
+			data=self._data[key]
+		except KeyError:
+			core.debug("[CAH]", "MISS", key)
+			raise CacheMissingException(key)
+
+		if data['expire'] <= int(time.time()):
+			self.delete(key)
+			raise CacheExpiredException(key)
+
+		core.debug("[CAH]", "HIT", key, data['value'])
+		return data['value']
+			
+	def delete(self, key):
+		"""Delete the value associated with the key."""
+		try:
+			del self._data[key]
+			core.debug("[CAH]", "DEL", key)
+		except KeyError:
+			pass
+		
+	def clear(self):
+		"""Clear the cache: erase all datas."""
+		self._data.clear()
+		core.debug("[CAH]", "Cleared")
+
+	def cleanup(self):
+		"""Delete outdated values."""
+		for key, data in self._data.items():
+			if data['expire'] <= int(time.time()):
+				self.delete(key)
+
+
+class CacheException(Exception):
+	"""Parent for all exceptions raised by the cache."""
+	pass
+
+class CacheMissingException(CacheException):
+	"""Raised when the asked value is unknown."""
+	pass
+
+class CacheExpiredException(CacheException):
+	"""Raised when the asked value is outdated."""
+	pass
+
+
+# vim: ts=4:sw=4:ai
---
+++ tests/datacache_test.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+# cxm - Clustered Xen Management API and tools
+# Copyleft 2011 - Nicolas AGIUS <nagius@astek.fr>
+
+###########################################################################
+#
+# This file is part of cxm.
+#
+# cxm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###########################################################################
+
+import cxm.datacache
+import unittest
+
+class DataCacheTests(unittest.TestCase):
+
+	def setUp(self):
+		self.cache=cxm.datacache.DataCache()
+
+	def test_get_ok(self):
+		value = "bla"
+		self.cache.add("test", 5, value)
+		self.assertEqual(self.cache.get("test"), value)
+		
+	def test_get_missing(self):
+		self.assertRaises(cxm.datacache.CacheMissingException, self.cache.get,"test")
+	
+	def test_get_outdated(self):
+		self.cache.add("test", -5, "value")
+		self.assertRaises(cxm.datacache.CacheExpiredException, self.cache.get,"test")
+		self.assertRaises(cxm.datacache.CacheMissingException, self.cache.get,"test")
+
+	def test_delete(self):
+		value = "bla"
+		self.cache.add("test1", 5, value)
+		self.cache.add("test2", 5, value)
+		self.cache.delete("test1")
+		self.assertRaises(cxm.datacache.CacheMissingException, self.cache.get,"test1")
+		self.assertEqual(self.cache.get("test2"), value)
+
+	def test_clear(self):
+		value = "bla"
+		self.cache.add("test1", 5, value)
+		self.cache.add("test2", 5, value)
+		self.cache.clear()
+		self.assertRaises(cxm.datacache.CacheMissingException, self.cache.get,"test1")
+		self.assertRaises(cxm.datacache.CacheMissingException, self.cache.get,"test2")
+	
+	def test_cleanup(self):
+		value = "bla"
+		self.cache.add("test1", -5, value)
+		self.cache.add("test2", 5, value)
+		self.cache.cleanup()
+		self.assertRaises(cxm.datacache.CacheMissingException, self.cache.get,"test1")
+		self.assertEqual(self.cache.get("test2"), value)
+
+if __name__ == "__main__":
+	unittest.main()   
+
+# vim: ts=4:sw=4:ai
